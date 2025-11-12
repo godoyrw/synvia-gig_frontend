c45e3b132dcaee5f04c4d9d8389ae5fcf44ccbe5
Roberto Godoy <godoyrw@gmail.com>
2025-11-12 13:34:35 -0300

    Tema limpo - Sakai

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        <span class="text-primary font-bold">SYNVIA™ | The Intelligent Infrastructure for Health Management</span>
+    </div>
+</template>
---
+++ src/views/Synvia-Gig.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import synviaGigLogo from '@/assets/images/logos/synvia_gig_positivo.png';
+</script>
+
+<template>
+    <div class="card">
+        <div class="font-semibold text-xl mb-4">
+            <img :src="synviaGigLogo" alt="Logo" class="w-60 mt-0" />
+        </div>
+    </div>
+    
+
+</template>
+
