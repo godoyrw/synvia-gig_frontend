b93769823033866a422888f2b7546a5edda8535e
Roberto Godoy <godoyrw@gmail.com>
2025-12-02 22:13:25 -0300

    Update de ML

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -27,6 +27,7 @@ declare module 'vue' {
     RouterView: typeof import('vue-router')['RouterView']
     SelectButton: typeof import('primevue/selectbutton')['default']
     Tag: typeof import('primevue/tag')['default']
+    Textarea: typeof import('primevue/textarea')['default']
     Toast: typeof import('primevue/toast')['default']
     ToggleSwitch: typeof import('primevue/toggleswitch')['default']
   }
