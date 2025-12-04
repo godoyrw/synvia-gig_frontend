a0a09588b71d99e29d76e33f3b3a25fba458c81a
Roberto Godoy <godoyrw@gmail.com>
2025-12-03 22:01:55 -0300

    Ajuste de env.d.ts

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string
   readonly VITE_USE_MOCK_USERS?: string
   readonly VITE_USE_MOCK_TENANTS?: string
   // adicione outras variáveis VITE_ aqui conforme for criando
